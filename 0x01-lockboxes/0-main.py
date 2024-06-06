a932a661ee2a8fb8213da8658c540378c0fe6353
Adejare <adejare.arasheed@gmail.com>
2024-06-06 09:39:01 +0100

    Main files

--- 0x01-lockboxes/0-main.py
+++ 0x01-lockboxes/0-main.py
@@ -10,3 +10,9 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+boxes = [[], [], [6, 1], [3]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 7], [3], [4, 1], [2]]
+print(canUnlockAll(boxes))
