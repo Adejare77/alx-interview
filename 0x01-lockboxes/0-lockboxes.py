a35056bd78c914ba482ecd42094a7a2e2993e8d6
Adejare <adejare.arasheed@gmail.com>
2024-06-06 08:06:44 +0100

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lock Boxes"""
+
+
+def canUnlockAll(boxes):
+    """Check if all Boxes can be Opened"""
+    keys = boxes[0]  # since the first box is always unlock
+    result = set(recursion(boxes, keys, 0))
+    result.add(0)  # include to result if not already present
+    box_length = set(range(len(boxes)))  # inorder to compare
+    if (box_length.issubset(result) or result == box_length):
+        return True
+    return False
+
+
+def recursion(boxes, keys, index):
+    if (index >= len(keys)):
+        return keys
+    key_idx = keys[index]
+
+    if key_idx >= len(boxes):
+        return keys
+    elif not (set(boxes[key_idx]).issubset(keys)):
+        diff = list(set(boxes[key_idx]).difference(set(keys)))
+        keys.extend(diff)
+        index += 1
+        return recursion(boxes, keys, index)
+    else:
+        index += 1
+        return recursion(boxes, keys, index)
