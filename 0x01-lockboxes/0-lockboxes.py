c2cdb2bfb8487e9b68b1fd6b6551eef1853fab8c
Adejare <adejare.arasheed@gmail.com>
2024-06-07 14:58:32 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,16 +7,20 @@ def canUnlockAll(boxes):
     if not boxes:
         return
     keys = boxes[0]  # since the first box is always unlock
-    result = set(recursion(boxes, keys, 0))
+    # unique_boxes to minimize number of recursion
+    unique_boxes = []
+    [unique_boxes.append(box) for box in boxes if not (box in unique_boxes)]
+    result = set(recursion(unique_boxes, keys, 0))
     result.add(0)  # include to result if not already present
-    box_length = set(range(len(boxes)))  # inorder to compare
+    box_length = set(range(len(unique_boxes)))  # inorder to compare
     if (box_length.issubset(result) or result == box_length):
         return True
     return False
 
 
 def recursion(boxes, keys, index):
-    if (index >= len(keys)):
+    # len(boxes) == 1, since first box is always open
+    if (index >= len(keys) or len(boxes) == 1):
         return keys
     key_idx = keys[index]
 
