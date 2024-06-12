d7664a2dc9af8edd81afa729c2eb0c6efd616a27
Adejare <adejare.arasheed@gmail.com>
2024-06-12 11:02:09 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -35,16 +35,13 @@ def recursive_operation(n):
             operations[1] = operations[1] + 'h'
         return operations
 
-    stored_cpy = 0
     h = len(operations[1])
     while h < n:
         if not n % h:  # if we are to multiply we'll use Copy and Paste
-            stored_cpy = h
             operations[0].extend(['Copy All', 'Paste'])
-            operations[1] = operations[1] + (stored_cpy * 'h')
-            h = h + stored_cpy
         else:
             operations[0].append('Paste')
-            operations[1] = operations[1] + (stored_cpy * 'h')
-            h = h + stored_cpy
+
+        operations[1] = operations[1] + (h * 'h')
+        h = 2 * h
     return operations
