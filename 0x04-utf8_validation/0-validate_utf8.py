cafa4d3bf529b954b19cb420a1230477bbb69a97
Adejare <adejare.arasheed@gmail.com>
2024-06-25 11:13:09 +0100

    utf-8 validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,32 +11,13 @@ Write a method that determines if a given data set represents a valid UTF-8 enco
     Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer
 """
 
-# def validUTF8(data):
-#     """determines if a given data set represents a valid UTF-8
-#     encoding. Return True if it is, else False
-#     """
-#     # For Ascii characters
-#     min_val = bin(0)
-#     max_val = bin(127)
-#     print(min_val, max_val)
-#     for values in data:
-#         print(values, bin(values))
-#         if not (bin(values) >= min_val and bin(values) <= max_val):
-#             return False
-#         str_val = str(values)
-#         # for character in str_val:
-#         #     print(character)
-#         #     print(ord(character))
-#         #     if not (bin(ord(character)) >= min_val and bin(ord(character)) <= max_val):
-#         #         return False
-#     return True
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8
     encoding. Return True if it is, else False
     """
     # For Ascii characters
-    min_val = 0
+    min_val = -127
     max_val = 127
     for values in data:
         if not (values >= min_val and values <= max_val):
