f6082d9432942cda9f3b96ae26821a13a5cb904a
Adejare <adejare.arasheed@gmail.com>
2024-08-01 07:54:00 +0100

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Island Permieter """
+
+
+def island_perimeter(grid):
+    """ finds the perimeter of an island """
+    island = 0
+    repated_edges = 0
+    for row_idx, rows in enumerate(grid):
+        for col_idx, cols in enumerate(rows):
+            if cols:
+                island += 1
+                if (col_idx - 1 >= 0 and rows[col_idx - 1]):
+                    repated_edges += 1
+                if (row_idx - 1 >= 0 and grid[row_idx - 1][col_idx]):
+                    repated_edges += 1
+
+    permieter_of_all_island = 4 * island - repated_edges * 2
+    return permieter_of_all_island
