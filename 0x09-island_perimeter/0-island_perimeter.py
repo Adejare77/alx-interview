dbc5cc0a1eb4c16b30d494605c536991e5983c86
Adejare <adejare.arasheed@gmail.com>
2024-08-01 07:57:54 +0100

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,9 +11,9 @@ def island_perimeter(grid):
             if cols:
                 island += 1
                 if (col_idx - 1 >= 0 and rows[col_idx - 1]):
-                    repated_edges += 1
+                    repated_edges += 2
                 if (row_idx - 1 >= 0 and grid[row_idx - 1][col_idx]):
-                    repated_edges += 1
+                    repated_edges += 2
 
-    permieter_of_all_island = 4 * island - repated_edges * 2
+    permieter_of_all_island = 4 * island - repated_edges
     return permieter_of_all_island
