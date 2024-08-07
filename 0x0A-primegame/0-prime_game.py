b484c82ed5f37092d287d1a3559495a54f77d014
Adejare <adejare.arasheed@gmail.com>
2024-08-07 17:07:26 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -26,55 +26,55 @@ def isPrime(n):
         return True
 
 
-# def isWinner(x, nums):
-#     """ Determine who is the winner between Maria and Ben """
-#     if not x or not nums:
-#         return None
-
-#     users = {'Maria': 0, 'Ben': 0}
-#     for round in nums:
-#         currentPlayer = True
-#         availableSlots = list(range(2, round + 1))
-#         while availableSlots:
-#             if isPrime(availableSlots[0]):
-#                 currentPlayer = not currentPlayer
-#             availableSlots.remove(availableSlots[0])
-#         if not currentPlayer:
-#             users['Maria'] += 1
-#         else:
-#             users['Ben'] += 1
-
-#     if users['Maria'] == users['Ben']:
-#         return None
-#     winner = max(users, key=users.get)
-#     return winner
-
 def isWinner(x, nums):
     """ Determine who is the winner between Maria and Ben """
     if not x or not nums:
         return None
 
-    def play_game(n):
-        availableSlots = set(range(2, n + 1))
-        currentPlayer = True  # True for Maria, false for Ben
-        while availableSlots:
-            # Find the smallest prime number
-            prime = min([num for num in availableSlots if
-                         isPrime(num)], default=None)
-            if not prime:
-                break
-            # Remove the prime and all its multiples
-            availableSlots.difference_update(range(prime, n + 1, prime))
-            currentPlayer = not currentPlayer
-
-        # Determine the winner based on who cannot make a move
-        return 'Maria' if not currentPlayer else 'Ben'
-
     users = {'Maria': 0, 'Ben': 0}
     for round in nums:
-        winner = play_game(round)
-        users[winner] += 1
+        currentPlayer = True
+        availableSlots = list(range(2, round + 1))
+        while availableSlots:
+            if isPrime(availableSlots[0]):
+                currentPlayer = not currentPlayer
+            availableSlots.remove(availableSlots[0])
+        if not currentPlayer:
+            users['Maria'] += 1
+        else:
+            users['Ben'] += 1
 
     if users['Maria'] == users['Ben']:
         return None
-    return max(users, key=users.get)
+    winner = max(users, key=users.get)
+    return winner
+
+# def isWinner(x, nums):
+#     """ Determine who is the winner between Maria and Ben """
+#     if not x or not nums:
+#         return None
+
+#     def play_game(n):
+#         availableSlots = set(range(2, n + 1))
+#         currentPlayer = True  # True for Maria, false for Ben
+#         while availableSlots:
+#             # Find the smallest prime number
+#             prime = min([num for num in availableSlots if
+#                          isPrime(num)], default=None)
+#             if not prime:
+#                 break
+#             # Remove the prime and all its multiples
+#             availableSlots.difference_update(range(prime, n + 1, prime))
+#             currentPlayer = not currentPlayer
+
+#         # Determine the winner based on who cannot make a move
+#         return 'Maria' if not currentPlayer else 'Ben'
+
+#     users = {'Maria': 0, 'Ben': 0}
+#     for round in nums:
+#         winner = play_game(round)
+#         users[winner] += 1
+
+#     if users['Maria'] == users['Ben']:
+#         return None
+#     return max(users, key=users.get)
